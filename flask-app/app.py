cb4fbb9c061652b35745877d7d60fdf471d798f3
Steven Johnson <steven.j2019@pm.me>
2020-06-30 12:31:38 +0100

    works

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+import os 
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://[USERNAME]:[PASSWORD]@mysql:3306/flask-db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('URI')
 
 class Users(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
